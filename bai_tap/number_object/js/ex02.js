39a7abeea489bdbfd651e1f3b32f84236fb955ab
VINH <ducvinh50@email.com>
2023-09-06 00:04:12 +0700

    Nop bai tap number object

--- bai_tap/number_object/js/ex02.js
+++ bai_tap/number_object/js/ex02.js
@@ -1,11 +1,6 @@
-// * Let, Course
-// 1. Var / Let, Const: Scope, Hosting
-// 2. Const / Var, Let:  Assignment
-
-// Code block: if else, loop, {},...
 var price = "123456896";
 Object.prototype.getCurrency = function (unit) {
-  if (!+this) return "Không phải là số";
+  if (!+this) return "Không phải số";
   var arr = this.toString().split("");
   for (var i = arr.length - 1; i >= 0; i -= 3) {
     if (i === arr.length - 1) continue;
